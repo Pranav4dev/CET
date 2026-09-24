7b2f457ff4ed5f714a5a0092942c902a5d367c99
Muhammed Sahal V C <pranav4dev@gmail.com>
2026-09-24 11:59:58 +0530

    13 line 5-6

--- Python/13.py
+++ Python/13.py
@@ -2,4 +2,5 @@ s1 = input("Enter the first string: ")
 s2 = input("Enter the second string: ")
 p = int(input("Enter the position: "))
 s3 = s1[:p] + s2[p] + s1[p+1:]
-print(s3)
+s4 = s2[:p] + s1[p] + s2[p+1:]
+print(s3+s4)
